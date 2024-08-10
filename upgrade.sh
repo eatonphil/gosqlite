4fde52889533162262d7a79a07c5889357b76c90
Chris Frank <cgfrank@gmail.com>
2024-08-10 08:29:50 -0400

    update to sqlite3 3.46.0

--- upgrade.sh
+++ upgrade.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-VERSION=3430200
+VERSION=3460000
 wrk="$(pwd)"
 pushd "$(mktemp -d)"
-wget "https://www.sqlite.org/2023/sqlite-src-$VERSION.zip"
+wget "https://www.sqlite.org/2024/sqlite-src-$VERSION.zip"
 unzip "sqlite-src-$VERSION.zip"
 cd "sqlite-src-$VERSION"
 CFLAGS='-DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1' ./configure
